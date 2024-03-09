e90214324035d40287315064d6cfe059e1cfc260
Marvin <gudiellopezmarvin@gmail.com>
2024-03-09 11:35:41 -0600

    commit 1 primer parcial

---
+++ ControlVersiones/ControlVersiones/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlVersiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es el primer parcial de programacion");
+            Console.ReadLine();
+        }
+    }
+}
